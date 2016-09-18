f1e29b7f252e9e9c2676e0fc8ba85ace9e7906a3
AJ Nead <aj@Jet.com>
2016-09-18 14:10:54 -0400

    first commit

---
+++ learn.scala
@@ -0,0 +1,51 @@
+/*
+var a: Double = 2.72
+
+a = 355.0 / 133.0 
+
+a = 5
+*/
+
+/*
+val b: Byte = 10
+
+val s: Short = b
+
+var d: Double = s
+
+var l: Long = 20 
+
+var i: Int = l.toInt
+*/
+
+/*
+val greeting: String = "Hello Lyla!"
+
+val matched: Boolean = (greeting == "Hello Lyla!")
+
+println(s"$greeting It's nice to see you today.")
+
+s"${greeting} It's nice to see you today."
+
+val item: String = "apple"
+
+f"I wrote a new $item%.3s today"
+*/
+
+/*
+val input: String = "Enjoying this apple 3.14159 times today"
+
+val pattern = """.* apple ([\d.]+) times.*""".r
+
+val pattern(amountText) = input
+
+val amount: Double = amountText.toDouble
+*/
+
+val c: Char = 'A'
+
+val i: Int = c
+
+val t: Char = 116
+
+
